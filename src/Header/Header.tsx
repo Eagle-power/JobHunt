e312d95122583f4af5177672d2cb0a4b2e6ec4c9
Ankur Kumar <ankurkumar352019@gmail.com>
2025-10-22 18:00:08 +0530

    Landing page updated

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,28 +1,30 @@
-import { Avatar } from "@mantine/core"
+import { Avatar, Indicator } from "@mantine/core"
 import { IconAnchor, IconBell, IconSettings2 } from "@tabler/icons-react"
+import NavLinks from "./NavLinks"
 
 const Header = () => {
     return (
-        <div className="flex w-full px-6 text-white bg-mine-shaft-950 h-28 justify-between items-center ">
-            <div className="flex gap-1 items-center text-bright-sun-400">
+        <div className="flex w-full px-6 text-white bg-mine-shaft-950 h-20 justify-between items-center ">
+            <div className="flex gap-1 items-center text-bright-sun-400 cursor-pointer " ri>
                 <IconAnchor className="h-8 w-8" stroke={2.5} />
                 <div className="text-3xl font-semibold">
                     Job Hunt
                 </div>
             </div>
-            <div className="flex gap-3">
-                <a href="">Find Jobs</a>
-                <a href="">Find Talent</a>
-                <a href="">Upload Job</a>
-                <a href="">About us</a>
-            </div>
+            {NavLinks()}
             <div className="flex  gap-5 items-center">
-                <IconBell />
                 <div className="flex items-center gap-2">
                     <div>Ankur</div>
                     <Avatar src="avatar.png" alt="Ankur" />
                 </div>
-                <IconSettings2 />
+                <div className="bg-mine-shaft-900 p-1.5 rounded-full">
+                    <IconSettings2 stroke={1.5} />
+                </div>
+                <div className="bg-mine-shaft-900 p-1.5 rounded-full">
+                    <Indicator  color="bright-sun.4" size={8} offset={6} processing>
+                        <IconBell stroke={1.5} />
+                    </Indicator>
+                </div>
             </div>
         </div>
     )
---
+++ src/LandingPage/Working.tsx
@@ -0,0 +1,42 @@
+import { Avatar } from "@mantine/core"
+import { work } from "../Data/Data"
+
+const Working = () => {
+    return (
+        <div className="mt-20 pb-5">
+            <div className="text-4xl mb-3 text-center font-bold text-mine-shaft-100 [&>span]:text-bright-sun-400">How it <span>Works</span></div>
+            <div className="text-lg mb-10 mx-auto text-mine-shaft-300 text-center w-1/2">Effortlessly navigate through the process  and land your dream job.</div>
+
+            <div className="flex   px-16 justify-between items-center">
+                <div className="relative">
+                    <img className="w-[40rem]" src="/Working/Girl.png" alt="How it works" />
+                    <div className="absolute  top-[20%] right-10 w-36 flex flex-col gap-1 items-center border border-bright-sun-400 py-3 px-1 rounded-lg backdrop-blur-md">
+                        <Avatar className="!h-16 !w-16" src='avatar2.png' alt='Girl' />
+                        <div className="text-sm font-semibold text-mine-shaft-200 text-center">Complete your profile</div>
+                        <div className="text-xs text-mine-shaft-300">70% Completed</div>
+                    </div>
+                </div>
+                <div className="flex flex-col  gap-10">
+
+                    {
+                        work.map((item, idx) => (
+                            <div key={idx} className="flex items-center gap-4 ">
+                                <div className="p-3 bg-bright-sun-300  rounded-full">
+                                    <img className="h-12  w-12" src={`/Working/${item.name}.png`} alt={item.name} />
+                                </div>
+                                <div>
+                                    <div className="text-mine-shaft-200 text-xl font-semibold">{item.name}</div>
+                                    <div className="text-mine-shaft-300">{item.desc}</div>
+                                </div>
+                            </div>
+                        ))
+                    }
+
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Working
